7169ffa7b0d87794440ddfa5eb634c99604df85b
ACfma <andrea.carli1996@libero.it>
2021-01-31 17:05:07 +0100

    Upgraded test

--- tests/test_import.py
+++ tests/test_import.py
@@ -7,19 +7,21 @@ import SimpleITK as sitk
 from Neuroimages_GM_AD_Detection.Brain_Sequence import Brain_Sequence
 
 class TestCore(unittest.TestCase):
-    '''Testing the core'''
+    '''Testing the import function'''
     PATH = os.path.abspath('')
     FILE = '\**\smwc1CTRL-1.nii'
     FILE = PATH+FILE
     image = sitk.ReadImage(glob.glob(os.path.normpath(FILE)), imageIO = "NiftiImageIO")
 
     def test_import_data(self):
-        '''Testing...'''
+        '''Testing the extraction'''
         self.assertTupleEqual(sitk.GetArrayFromImage(self.image).shape,(1,121,145,121))
 
     def test_brain_sequence(self):
         '''Test for user-defined function'''
         self.assertEqual(len(Brain_Sequence('Axial', sitk.GetArrayFromImage(self.image)[0,:,:,:])),121)
+        self.assertEqual(len(Brain_Sequence('Coronal', sitk.GetArrayFromImage(self.image)[0,:,:,:])),145)
+        self.assertEqual(len(Brain_Sequence('Sagittal', sitk.GetArrayFromImage(self.image)[0,:,:,:])),121)
 
 if __name__=='__main__':
     unittest.main()
