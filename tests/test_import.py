5e8d773e55e90646297fae454ea4af989951e2ae
ACfma <andrea.carli1996@libero.it>
2021-02-01 12:15:15 +0100

    Updating tests...

--- tests/test_import.py
+++ tests/test_import.py
@@ -9,7 +9,7 @@ from Neuroimages_GM_AD_Detection.Brain_Sequence import Brain_Sequence
 class TestCore(unittest.TestCase):
     '''Testing the import function'''
     #PATH = os.path.abspath('')
-    FILE = 'smwc1CTRL-1.nii'
+    FILE = '/tests/smwc1CTRL-1.nii'
     #FILE = PATH+FILE
     #print(glob.glob(os.path.normpath(FILE)))
     image = sitk.ReadImage(FILE, imageIO = "NiftiImageIO")
